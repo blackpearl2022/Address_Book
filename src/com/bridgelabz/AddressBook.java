3baddc50aba96887b9d33f3066f91ad18d28e165
Aditya Bahakar <adityabahakar99@gmail.com>
2022-05-26 22:54:09 +0530

    Ability to add new contact to address book

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -9,4 +9,18 @@ public class AddressBook {
     public void createContact(Contact contact) {
         contacts.add(contact);
     }
+
+    public void displayContacts() {
+        for (int i = 0; i < contacts.size(); i++) {
+
+            System.out.println("first name="+contacts.get(i).getFirstName());
+            System.out.println("Last name="+contacts.get(i).getLastName());
+            System.out.println("address="+contacts.get(i).getAddress());
+            System.out.println("City name="+contacts.get(i).getCity());
+            System.out.println("State name="+contacts.get(i).getState());
+            System.out.println("zip code="+contacts.get(i).getZip());
+            System.out.println("Phone number="+contacts.get(i).getPhoneNumber());
+            System.out.println("Email="+contacts.get(i).getEmail());
+        }
+    }
 }
--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -1,7 +1,49 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book program.");
+
+        System.out.println("Enter the first name: ");
+        Scanner sc1 = new Scanner(System.in);
+        String first_name = sc1.next();
+
+        System.out.println("Enter the last name: ");
+        Scanner sc2 = new Scanner(System.in);
+        String last_name = sc2.next();
+
+        System.out.println("Enter the address: ");
+        Scanner sc3 = new Scanner(System.in);
+        String address = sc3.next();
+
+        System.out.println("Enter the city name: ");
+        Scanner sc4 = new Scanner(System.in);
+        String city = sc4.next();
+
+        System.out.println("Enter the state's name: ");
+        Scanner sc5 = new Scanner(System.in);
+        String state = sc5.next();
+
+        System.out.println("Enter the zip: ");
+        Scanner sc6 = new Scanner(System.in);
+        String zip = sc6.next();
+
+        System.out.println("Enter the phone number: ");
+        Scanner sc7 = new Scanner(System.in);
+        String phone_num = sc7.next();
+
+        System.out.println("Enter the email ID: ");
+        Scanner sc8 = new Scanner(System.in);
+        String email = sc8.next();
+
+        AddressBook addressBook = new AddressBook();
+        addressBook.createContact(
+                new Contact(first_name, last_name, address, city, state,
+                        zip, phone_num, email));
+
+        addressBook.displayContacts();
     }
+
 }
