015f8327abed4b2e18737e23ff5b0407e0a303a5
Aditya Bahakar <adityabahakar99@gmail.com>
2022-05-28 01:06:58 +0530

    Abitlity to delete contact using neme

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -15,14 +15,14 @@ public class AddressBook {
         for (int i = 0; i < contacts.size(); i++) {
 
             Contact contact = contacts.get(i);
-            System.out.println("first name="+ contact.getFirstName());
-            System.out.println("Last name="+ contact.getLastName());
-            System.out.println("address="+ contact.getAddress());
-            System.out.println("City name="+ contact.getCity());
-            System.out.println("State name="+ contact.getState());
-            System.out.println("zip code="+ contact.getZip());
-            System.out.println("Phone number="+ contact.getPhoneNumber());
-            System.out.println("Email="+ contact.getEmail());
+            System.out.println("first name=" + contact.getFirstName());
+            System.out.println("Last name=" + contact.getLastName());
+            System.out.println("address=" + contact.getAddress());
+            System.out.println("City name=" + contact.getCity());
+            System.out.println("State name=" + contact.getState());
+            System.out.println("zip code=" + contact.getZip());
+            System.out.println("Phone number=" + contact.getPhoneNumber());
+            System.out.println("Email=" + contact.getEmail());
         }
     }
 
@@ -31,34 +31,52 @@ public class AddressBook {
         Scanner sc1 = new Scanner(System.in);
 
         for (int i = 0; i < contacts.size(); i++) {
-            if(contacts.get(i).getFirstName().equals(firstName)){
+            if (contacts.get(i).getFirstName().equals(firstName)) {
 
                 System.out.println("Enter the name to edit First name");
                 String newFirstName = sc1.next();
+                contacts.get(i).setFirstName(newFirstName);
 
                 System.out.println("Enter the name to edit Last name");
                 String newLastName = sc1.next();
+                contacts.get(i).setLastName(newLastName);
 
                 System.out.println("Enter the name to edit Address");
                 String newAddress = sc1.next();
+                contacts.get(i).setAddress(newAddress);
 
                 System.out.println("Enter the name to edit City name");
                 String newCity = sc1.next();
+                contacts.get(i).setCity(newCity);
 
                 System.out.println("Enter the name to edit State name");
                 String newState = sc1.next();
+                contacts.get(i).setState(newState);
 
                 System.out.println("Enter the name to edit Zip code");
                 String newZip = sc1.next();
+                contacts.get(i).setZip(newZip);
 
                 System.out.println("Enter the name to edit Phone Number");
                 String newPhoneNumber = sc1.next();
+                contacts.get(i).setPhoneNumber(newPhoneNumber);
 
                 System.out.println("Enter the name to edit Email");
                 String newEmail = sc1.next();
-            }
-            else {
+                contacts.get(i).setEmail(newEmail);
+            } else {
                 System.out.println("Contact to edit not found");
+            }
+        }
+    }
+
+    public void deleteContact(String firstName) {
+        for (int i = 0; i < contacts.size(); i++) {
+            if (contacts.get(i).getFirstName().equals(firstName)) {
+
+                contacts.remove(i);
+
+                System.out.println("Contact with name " + firstName + " is deleted");
             }
         }
     }
--- src/com/bridgelabz/Contact.java
+++ src/com/bridgelabz/Contact.java
@@ -2,6 +2,39 @@ package com.bridgelabz;
 
 public class Contact {
     private String firstName;
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     private String lastName;
     private String address;
     private String city;
